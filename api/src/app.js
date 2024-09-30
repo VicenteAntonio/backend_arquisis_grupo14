49bcff72a70d912227c5b215eac94ea5b2da5e62
mane6867 <magdalena.pino@uc.cl>
2024-09-30 19:58:13 -0300

    se añade cors

--- api/src/app.js
+++ api/src/app.js
@@ -3,12 +3,14 @@ const { Op } = require('sequelize');
 const KoaLogger = require('koa-logger');
 const { koaBody } = require('koa-body');
 const router = require('./routes.js');
+const cors = require('@koa/cors')
 const orm = require('./models');
 
 const app = new koa();
 
 app.context.orm = orm;
 
+app.use(cors());
 // middlewares
 app.use(KoaLogger());
 app.use(koaBody());

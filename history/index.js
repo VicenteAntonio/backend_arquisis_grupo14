572c518ee95e7174a59a891aed4d06bdd90fff09
solebravo <solebravo@uc.cl>
2024-10-02 02:19:10 -0300

    feat: listener hystory

---
+++ history/index.js
@@ -0,0 +1,62 @@
+const mqtt = require('mqtt');
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+dotenv.config();
+
+const HOST = 'broker.iic2173.org';
+const PORT = 9000;  // El puerto sigue siendo 9000, ajustalo si es necesario
+const USER = 'students';
+const PASSWORD = 'iic2173-2024-2-students';
+const TOPIC = 'fixtures/history';  // Tópico actualizado
+
+const client = mqtt.connect(`mqtt://${HOST}:${PORT}`, {
+    username: USER,
+    password: PASSWORD
+});
+
+client.on('connect', () => {
+    console.log('Connected to MQTT broker');
+    client.subscribe(TOPIC, (err) => {
+        if (err) {
+            console.error('Error subscribing to topic', err);
+        } else {
+            console.log(`Subscribed to topic ${TOPIC}`);
+        }
+    });
+});
+
+function parseFixtureData(fixtureData) {
+  try {
+    const fixturesArray = JSON.parse(fixtureData);
+    return fixturesArray;
+  } catch (error) {
+    console.error('Error parsing fixture data: ', error);
+  }
+}
+
+async function sendFixtureToAPI(fixture) {
+    try {
+      const response = await axios.post(`${process.env.API_URL}/fixtures/history`, fixture);  // Asegúrate de que la ruta en la API sea la correcta
+      console.log('Fixture history sent to API: ', response.data);
+    } catch (error) {
+      console.error('Error sending fixture history to API: ', error);
+    }
+}
+
+client.on('message', (topic, message) => {
+    console.log(`Received message on ${topic}`);
+    try {
+      const fixtureData = JSON.parse(message.toString());
+      const fixtureList = parseFixtureData(fixtureData);
+      sendFixtureToAPI(fixtureList);
+    } catch (error) {
+      console.error('Error parsing message or sending fixture to API: ', error);
+    }
+});
+
+client.on('error', (error) => {
+    console.error('Error connecting to MQTT broker: ', error);
+});
+
+module.exports = client;

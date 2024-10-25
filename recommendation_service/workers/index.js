d3446ec7958b85fe36c0ae3e8f3c1d5a8a9fdb9d
Francisca <fccares@uc.cl>
2024-10-24 22:48:33 -0300

    implement woker functions(preliminar)

--- recommendation_service/workers/index.js
+++ recommendation_service/workers/index.js
@@ -61,16 +61,140 @@ async function getAllPurchasesResults(username) {
 
 // Luego, se obtienen los partidos de los equipos que estaban involucrados en todas sus compras (importante que aparezcan por liga)
 
-async function getMatchesByTeams(purchases) {}
+async function getMatchesByTeams(purchases) {
+  try {
+    const teams = purchases.map((purchase) => purchase.fixture.homeTeam);
+    // important to get the teams by league
+    const leagues = purchases.map((purchase) => purchase.leagueName);
+
+    const matches = await Fixture.findAll({
+      where: {
+        [Op.or]: [
+          {
+            homeTeam: {
+              [Op.in]: teams,
+            },
+          },
+          {
+            awayTeam: {
+              [Op.in]: teams,
+            },
+          },
+        ],
+        league: {
+          [Op.in]: leagues,
+        },
+      },
+    });
+
+    return matches;
+  } catch (error) {
+    console.log(`Error getting matches by teams: ${error.message}`);
+    throw error;
+  }
+}
 
 // después, se calcula cuantas veces ha acertado en todas las apuestas a los resultados cuando uno de los equipos de
 // los proximos partidos ha estado involucrado, tomando en cuenta cuantas veces apostó en el mismo partido.
 
-async function calculateAsserts(username, matches) {}
+async function calculateAsserts(username, matches) {
+  try {
+    const currentDate = new Date(); // fecha actual
 
-// Luego, se calculan los beneficios de los partidos para cada liga tomando en cuenta la cantidad de aciertos antes calculada,  la cantidad de partidos de los equipos con los que acerto y la probabilidad.
+    const user = await User.findOne({
+      // obtener usuario por username
+      where: { username }, // username
+      include: [
+        {
+          model: Request, // modelo de request
+          where: {
+            //   status: {
+            //     [Op.or] : [],
+            //   }, // status de la request es completa
+            datetime: {
+              [Op.lt]: currentDate, // fecha menor que la actual
+            },
+          },
+        },
+      ],
+    });
 
-async function calculatePonderation(asserts, round, odds) {}
+    if (!user) {
+      throw new Error('User not found');
+    }
+    // reviso los acertados, Winner es correcto
+
+    const asserts = matches.map((match) => {
+      const matchPurchases = user.requests.filter(
+        (purchase) => purchase.fixture_id === match.id
+      );
+
+      const matchAsserts = matchPurchases.filter(
+        (purchase) => purchase.result === match.result
+      );
+
+      return {
+        match, // partido
+        asserts: matchAsserts.length, // cantidad de aciertos
+      };
+    });
+
+    return asserts; // retorno los aciertos
+  } catch (error) {
+    console.log(`Error calculating asserts: ${error.message}`);
+    throw error;
+  }
+}
+
+// Luego, se calculan los beneficios de los partidos para cada liga tomando en cuenta la cantidad de aciertos antes calculada,
+// la cantidad de partidos de los equipos con los que acerto y la probabilidad.
+async function calculatePonderation(asserts, round, odds) {
+  try {
+    // asserts*league_round/(sum of odds)
+
+    const ponderation = asserts.map((assert) => {
+      const matchOdds = odds.find((odd) => odd.fixture.id === assert.match.id);
+      const matchPurchases = assert.matchPurchases;
+      const matchTeams = [
+        assert.match.homeTeamName, // ver si recibe id, name o logo
+        assert.match.awayTeamName, // ver si recibe id, name o logo
+      ];
+
+      const matchOddsSum = matchOdds.odds.reduce((acc, odd) => acc + odd, 0);
+
+      const matchPurchasesSum = matchPurchases.reduce(
+        (acc, purchase) => acc + purchase.quantity,
+        0
+      );
+
+      const matchPurchasesTeams = matchPurchases.reduce((acc, purchase) => {
+        if (matchTeams.includes(purchase.fixture.homeTeam)) {
+          acc.push(purchase.fixture.homeTeam);
+        }
+        if (matchTeams.includes(purchase.fixture.awayTeam)) {
+          acc.push(purchase.fixture.awayTeam);
+        }
+        return acc;
+      }, []);
+
+      const matchPurchasesTeamsSum = matchPurchasesTeams.length;
+
+      const pond =
+        (assert.asserts * round) /
+        (matchOddsSum + matchPurchasesTeamsSum + matchPurchasesSum);
+
+      return {
+        fixture: assert.match,
+        pond,
+      };
+    });
+
+    return ponderation;
+  } catch (error) {
+    console.log(`Error calculating ponderation: ${error.message}`);
+    throw error;
+  }
+}
 
 // Finalmente, se ordenan los partidos por beneficios y se devuelven los primeros 3
 

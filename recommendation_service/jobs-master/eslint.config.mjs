46fa00d421b74b2eb3438abf3b60c3a9fa4c60a6
VicenteAntonio <vicente.antonio@uc.cl>
2024-12-09 09:46:37 -0300

    global linter

--- recommendation_service/jobs-master/eslint.config.mjs
+++ recommendation_service/jobs-master/eslint.config.mjs
@@ -1,11 +1,19 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,jsx}'] },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      env: {
+        node: true, 
+      },
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
